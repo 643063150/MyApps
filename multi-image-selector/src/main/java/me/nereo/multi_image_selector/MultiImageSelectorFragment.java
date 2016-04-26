a05de66a8dde69a47c59eecc79f97a6935fbe198
643063150 <643063150@qq.com>
2016-04-26 14:14:26 +0800

    修改一个bug

--- multi-image-selector/src/main/java/me/nereo/multi_image_selector/MultiImageSelectorFragment.java
+++ multi-image-selector/src/main/java/me/nereo/multi_image_selector/MultiImageSelectorFragment.java
@@ -183,7 +183,11 @@ public class MultiImageSelectorFragment extends Fragment {
                 if (mImageAdapter.isShowCamera()) {
                     // 如果显示照相机，则第一个Grid显示为照相机，处理特殊逻辑
                     if (i == 0) {
-                        showCameraAction();
+                        if (mDesireImageCount == resultList.size()){
+                            Toast.makeText(getActivity(), R.string.msg_amount_limit, Toast.LENGTH_SHORT).show();
+                        }else {
+                            showCameraAction();
+                        }
                     } else {
                         // 正常操作
                         Image image = (Image) adapterView.getAdapter().getItem(i);
