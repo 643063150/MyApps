3c3b542735dc1f8cf4256665965deb8ef794c5b3
643063150 <643063150@qq.com>
2016-04-27 17:32:20 +0800

    时间选择控件

--- app/src/main/java/com/zpp/myapps/mainfragment/FourthFragment.java
+++ app/src/main/java/com/zpp/myapps/mainfragment/FourthFragment.java
@@ -6,14 +6,22 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.bigkoo.pickerview.TimePickerView;
 import com.zpp.myapps.R;
+import com.zpp.myapps.utils.mDateUtil;
+
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by admins on 2016/4/21.
  */
-public class FourthFragment extends Fragment {
+public class FourthFragment extends Fragment implements View.OnClickListener {
     View view;
+    TextView timepicker;
+    TimePickerView pvTime;
 
     public static FourthFragment instance() {
         FourthFragment view = new FourthFragment();
@@ -23,13 +31,36 @@ public class FourthFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.homefragment, null);
-//        intview();
+        view = inflater.inflate(R.layout.fourth_fragment, null);
+        intview();
 
         return view;
     }
 
     private void intview() {
+        //时间选择器
+        pvTime = new TimePickerView(getActivity(), TimePickerView.Type.YEAR_MONTH_DAY);
+        timepicker = (TextView) view.findViewById(R.id.timepicker);
+        timepicker.setOnClickListener(this);
+        Calendar calendar = Calendar.getInstance();
+        pvTime.setRange(calendar.get(Calendar.YEAR) - 70, calendar.get(Calendar.YEAR));
+        pvTime.setTime(new Date());
+        pvTime.setCyclic(false);
+        pvTime.setCancelable(true);
+        pvTime.setOnTimeSelectListener(new TimePickerView.OnTimeSelectListener() {
+            @Override
+            public void onTimeSelect(Date date) {
+                timepicker.setText(mDateUtil.getTime(date));
+            }
+        });
+    }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.timepicker:
+                pvTime.show();
+                break;
+        }
     }
 }
